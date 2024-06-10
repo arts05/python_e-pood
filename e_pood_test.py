e939cfda7abce946305ec994af81193cd2607b97
arts05 <131612995+arts05@users.noreply.github.com>
2024-06-10 18:22:14 +0300

    Implemented new tests and accommodated the shop history feature

--- e_pood_test.py
+++ e_pood_test.py
@@ -6,6 +6,7 @@ class TestShopping(unittest.TestCase):
         self.item1 = Item('Apple', 10, 5)
         self.item2 = Item('Banana', 20, 0)
         self.customer = Customer(1, 'Regular Customer', 100)
+        self.shop = Shop()
 
     def test_add_item(self):
         self.customer.basket.add_item(self.item1)
@@ -26,19 +27,31 @@ class TestShopping(unittest.TestCase):
 
     def test_purchase(self):
         self.customer.basket.add_item(self.item1)
-        self.customer.purchase()
+        self.customer.purchase(self.shop)
         self.assertEqual(self.customer.balance, 90)
         self.assertEqual(len(self.customer.history), 1)
 
+    def test_purchase_exceeds_balance(self):
+        self.customer.basket.add_item(self.item1)
+        self.customer.balance = 5
+        with self.assertRaises(Exception):
+            self.customer.purchase(self.shop)
+
+    def test_golden_customer_discount(self):
+        golden_customer = Customer(2, 'Golden Customer', 100)
+        golden_customer.basket.add_item(self.item1)
+        golden_customer.purchase(self.shop)
+        self.assertEqual(golden_customer.balance, 91)
+
     def test_purchase_empty_basket(self):
         with self.assertRaises(Exception):
-            self.customer.purchase()
+            self.customer.purchase(self.shop)
 
     def test_purchase_insufficient_balance(self):
         self.customer.basket.add_item(self.item1)
         self.customer.balance = 5
         with self.assertRaises(Exception):
-            self.customer.purchase()
+            self.customer.purchase(self.shop)
 
 if __name__ == '__main__':
     unittest.main()
